fdd65e86106fe97759eaad2cbb070944db0e8ed2
Allan Sims <allan.sims@envir.ee>
2020-11-24 13:21:37 +0200

    Updates to repo 24.11.2020

--- uuenda.cmd
+++ uuenda.cmd
@@ -1,5 +1,8 @@
+rem git remote add upstream https://github.com/OSGeo/grass-addons.git
+rem git fetch upstream
+
 git checkout master
 git merge upstream/master
 git push origin
-git commit -a -m "Updates to repo"
+git commit -a -m "Updates to repo %date%"
 git push origin
